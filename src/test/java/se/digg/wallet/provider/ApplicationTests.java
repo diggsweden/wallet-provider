f5749e452edef77a2dda1aefc5bd899eb4d2314a
Eric Thelin <extern.eric.thelin@digg.se>
2025-09-17 18:15:21 +0200

    test: check that we are up and running
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- src/test/java/se/digg/wallet/provider/ApplicationTests.java
+++ src/test/java/se/digg/wallet/provider/ApplicationTests.java
@@ -4,11 +4,33 @@
 package se.digg.wallet.provider;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
+  @Autowired
+  private MockMvc mvc;
+
   @Test
-  void contextLoads() {}
+  void isHealthy() throws Exception {
+    mvc.perform(get("/actuator/health")
+        .accept(MediaType.APPLICATION_JSON))
+        .andDo(log())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.status", is("UP")));
+  }
 }
