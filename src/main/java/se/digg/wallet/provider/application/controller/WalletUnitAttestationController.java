70405a2a8b39c80d0bcf3ba43e992ba48be640c0
Christoffer Killander <extern.christoffer.killander@digg.se>
2025-09-22 12:00:27 +0200

    feat: wua endpoint returns public key
    Signed-off-by: Christoffer Killander <extern.christoffer.killander@digg.se>

--- src/main/java/se/digg/wallet/provider/application/controller/WalletUnitAttestationController.java
+++ src/main/java/se/digg/wallet/provider/application/controller/WalletUnitAttestationController.java
@@ -4,57 +4,55 @@
 
 package se.digg.wallet.provider.application.controller;
 
+import com.nimbusds.jose.jwk.ECKey;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.digg.wallet.provider.application.model.WalletUnitAttestationDto;
 
 @RequestMapping("/wallet-unit-attestation")
 @RestController
-
 public class WalletUnitAttestationController {
 
-  private String createWuaResponse = """
-      {
-        "iss": "Digg",
-        "iat": 1516247022,
-        "exp": 1541493724,
-        "eudi_wallet_info": {
-          "general_info": {
-            "wallet_provider_name": "Digg",
-            "wallet_solution_id": "Diggidigg-id",
-            "wallet_solution_version": "0.0.1",
-            "wallet_solution_certification_information": "UNCERTIFIED"
-          },
-          "wscd_info": {
-            "wscd_type": "REMOTE",
-            "wscd_certification_information": "UNCERTIFIED",
-            "wscd_attack_resistance": 2
-          }
-        },
-        "status": {
-          "status_list": {
-            "idx": 412,
-            "uri": "https://revocation_url/statuslists/1"
-          }
-        },
-        "attested_keys": [
+  private final String createWuaResponse =
+      """
           {
-            "kty": "EC",
-            "crv": "P-256",
-            "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
-            "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ"
+            "iss": "Digg",
+            "iat": 1516247022,
+            "exp": 1541493724,
+            "eudi_wallet_info": {
+              "general_info": {
+                "wallet_provider_name": "Digg",
+                "wallet_solution_id": "Diggidigg-id",
+                "wallet_solution_version": "0.0.1",
+                "wallet_solution_certification_information": "UNCERTIFIED"
+              },
+              "wscd_info": {
+                "wscd_type": "REMOTE",
+                "wscd_certification_information": "UNCERTIFIED",
+                "wscd_attack_resistance": 2
+              }
+            },
+            "status": {
+              "status_list": {
+                "idx": 412,
+                "uri": "https://revocation_url/statuslists/1"
+              }
+            },
+            "attested_keys": [
+              %s
+            ]
           }
-        ]
-      }
-            """;
+          """;
 
   @PostMapping
   public ResponseEntity<String> postWalletUnitAttestation(
-      WalletUnitAttestationDto walletUnitAttestationDto) {
+      @RequestBody WalletUnitAttestationDto walletUnitAttestationDto) throws Exception {
 
+    ECKey key = ECKey.parse(walletUnitAttestationDto.jwk());
 
-    return ResponseEntity.ok(createWuaResponse);
+    return ResponseEntity.ok(String.format(createWuaResponse, key.toString()));
   }
 }
