13533b5a2dda1f9d639610d73d567435fa3aaa13
jahwag <540380+jahwag@users.noreply.github.com>
2025-09-23 11:35:03 +0200

    test: verify claims in WalletUnitAttestationServiceTest
    Signed-off-by: jahwag <540380+jahwag@users.noreply.github.com>

--- src/test/java/se/digg/wallet/provider/application/service/WalletUnitAttestationServiceTest.java
+++ src/test/java/se/digg/wallet/provider/application/service/WalletUnitAttestationServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.crypto.ECDSAVerifier;
 import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
@@ -15,6 +16,7 @@ import com.nimbusds.jwt.SignedJWT;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.ECPublicKey;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -42,6 +44,37 @@ class WalletUnitAttestationServiceTest {
 
     assertNotNull(jwt);
     assertEquals("Digg", jwt.getJWTClaimsSet().getIssuer());
+
+    verifyAttestedKeysClaim(jwt, jwk);
+    verifyEudiWalletInfoClaim(jwt);
+    verifyStatusClaim(jwt);
+    verifyJwtSignature(jwt, keystoreProperties.getPublicKey());
+  }
+
+  @SuppressWarnings("unchecked")
+  private static void verifyStatusClaim(SignedJWT jwt) throws ParseException {
+    Map<String, Object> status = jwt.getJWTClaimsSet().getJSONObjectClaim("status");
+    Map<String, Object> statusList = (Map<String, Object>) status.get("status_list");
+    assertEquals(412, statusList.get("idx"));
+    assertEquals("https://revocation_url/statuslists/1", statusList.get("uri"));
+  }
+
+  @SuppressWarnings("unchecked")
+  private static void verifyEudiWalletInfoClaim(SignedJWT jwt) throws ParseException {
+    Map<String, Object> eudiWalletInfo =
+        jwt.getJWTClaimsSet().getJSONObjectClaim("eudi_wallet_info");
+    Map<String, Object> generalInfo = (Map<String, Object>) eudiWalletInfo.get("general_info");
+    assertEquals("Diggidigg-id", generalInfo.get("wallet_solution_id"));
+    assertEquals("Digg", generalInfo.get("wallet_provider_name"));
+    assertEquals("0.0.1", generalInfo.get("wallet_solution_version"));
+    assertEquals("UNCERTIFIED", generalInfo.get("wallet_solution_certification_information"));
+
+    Map<String, Object> wscdInfo = (Map<String, Object>) eudiWalletInfo.get("wscd_info");
+    assertEquals("UNCERTIFIED", wscdInfo.get("wscd_certification_information"));
+  }
+
+  @SuppressWarnings("unchecked")
+  private static void verifyAttestedKeysClaim(SignedJWT jwt, ECKey jwk) throws ParseException {
     assertNotNull(jwt.getJWTClaimsSet().getClaim("attested_keys"));
 
     List<Map<String, Object>> attestedKeys =
@@ -50,8 +83,9 @@ class WalletUnitAttestationServiceTest {
     assertEquals(jwk.getX().toString(), attestedKey.get("x"));
     assertEquals(jwk.getY().toString(), attestedKey.get("y"));
     assertEquals(jwk.getCurve().toString(), attestedKey.get("crv"));
+  }
 
-    // Verify JWT signature using the certificate public key
-    assertTrue(jwt.verify(new ECDSAVerifier(keystoreProperties.getPublicKey())));
+  private void verifyJwtSignature(SignedJWT jwt, ECPublicKey publicKey) throws JOSEException {
+    assertTrue(jwt.verify(new ECDSAVerifier(publicKey)));
   }
 }
